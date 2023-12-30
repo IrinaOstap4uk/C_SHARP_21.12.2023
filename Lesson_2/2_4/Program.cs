f93fe295ad94ba129289cac2dccf808707e09e4d
Irina Ostapchuk <2376706@gmail.com>
2023-12-30 19:31:35 +0300

    Lessom 2 Task4

---
+++ Lesson_2/2_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// 4. Напишите программу, которая выводит третью с конца
+//    цифру заданного числа или сообщает, что третьей цифры
+//    нет.
+
+Console.WriteLine("Введите число");
+
+int a = int.Parse(Console.ReadLine()!);
+
+if (a >= 100)
+{
+    int num = a/100 %10;
+    Console.WriteLine(num);
+}
+else
+    if (a < 100 || a > 1000)
+{
+    Console.WriteLine("Нет третьей цифры");
+}
