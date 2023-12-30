7262aa34d4a5b3db177589cd7f2bdc91673f5153
Irina Ostapchuk <2376706@gmail.com>
2023-12-30 20:24:36 +0300

    Lesson 2 HW1

---
+++ Lesson_2/HW/HW_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// 1. Напишите программу, которая принимает 
+//    на вход число и проверяет, кратно ли оно
+//    одновременно 7 и 23.
+
+Console.WriteLine("Введите первое число");
+int num = Convert.ToInt32(Console.ReadLine()!);
+
+if (num % 7 == 0 && num % 23 == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+    Console.WriteLine("НеКратно ");
