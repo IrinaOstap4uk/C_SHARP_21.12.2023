c0a552fc7299bcf265cda64bd4abfb3e0e52ebd6
Irina Ostapchuk <2376706@gmail.com>
2023-12-31 01:37:58 +0300

    Lesson 2 HW3

---
+++ Lesson_2/HW/HW_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// 3. Напиши программу, которая принимает 
+//   на вход целое число из отрезка [10, 99] и показывает
+//   наибольшую цифру числа.
+
+Console.Write("Введите целое двухзначное число");
+int A = int.Parse(Console.ReadLine()!);
+
+if (A >= 10 && A <= 99)
+    int num1 = A / 10 == max;
+    int num2 = A % 10;
+    Console.WriteLine("max");
+
+else 
+    Console.WriteLine("num2");
